e092e2473a69078ac1852cc32d065b0f48e649c2
dhuntley <supaklutz@hotmail.com>
2019-07-22 22:16:22 -0600

    Simple client

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import SockJS from "sockjs-client";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            isConnected: false
+        };
+    }
+
+    handleClickConnect = () => {
+        const webSocket = new WebSocket("ws://localhost:8080/player");
+       
+        webSocket.onmessage = (data) => {
+            console.error(data);
+        };
+        
+        webSocket.onclose = () => {
+            this.updateConnectedState(false);
+            this.webSocket = null;
+        };
+
+        this.webSocket = webSocket;
+
+        this.updateConnectedState(true);
+    }
+
+    handleClickSendMessage = () => {
+        this.webSocket.send(JSON.stringify({}));
+    }
+
+    handleClickDisconnect = () => {
+        this.webSocket.close();
+        this.webSocket = null;
+
+        this.updateConnectedState(false);
+    }
+
+    updateConnectedState = (isConnected) => {
+        this.setState({isConnected});
+    }
+
+    render() {
+        return (
+            <div>
+                <div>My Happy Page</div>
+                {
+                    !this.state.isConnected ?
+                        <button onClick={this.handleClickConnect}>Connect</button> :
+                        <div>
+                            <button onClick={this.handleClickSendMessage}>Send Message</button>
+                            <button onClick={this.handleClickDisconnect}>Disconnect</button>
+                        </div>
+                }
+            </div>
+        );
+    }
 }
-
-export default App;
